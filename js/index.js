e556b5c2ce5709744897f84719d48cc2ed061f98
Ignacio <ignaciocharlin1995@gmail.com>
2021-03-13 11:30:32 -0300

    Subiendo nav

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){ 
+    $(".desplegable").click(function(){
+      if($('.navegacion').is(":visible")){
+          $('.navegacion').slideUp();
+      }else{
+        $('.navegacion').slideDown();
+      }
+    });
+    $(window).resize(function(){
+        if($(this).width()>768){
+            $(".navegacion").show();
+        } else{
+            $(".navegacion").hide();
+        }
+    });
+});
